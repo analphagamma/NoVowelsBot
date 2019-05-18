3bb0c64ca7acc16887eed672f41082504a435083
Peter Bocz <bocz.petya@gmail.com>
2019-05-18 23:13:43 +0100

    Added workflow

--- NoVowelsBot.py
+++ NoVowelsBot.py
@@ -1,5 +1,16 @@
 import praw
 import json
+from datetime import datetime, timedelta
+import sys
+from pprint import pprint
+
+from modules.BotEngine import BotEngine
+
+# constants
+SUBREDDITS = ['all']
+SCORE_THRESHOLD = 1000
+YDAY = (datetime.today() - timedelta(days=1)).timestamp() # yesterday 00:00 in UNIX timestamp
+NO_OF_POSTS = 5
 
 # getting login information
 class NoVowelsBot():
@@ -37,19 +48,19 @@ class NoVowelsBot():
             None in [login.get('client_id'),
                      login.get('client_secret'),
                      login.get('user_agent'),
-                     login.get('user_name'),
+                     login.get('username'),
                      login.get('password')]:
             raise IncompleteLoginDetailsError
-            
+
         return login
     
-    def reddit_instance(self):
+    def init_bot(self):
         login = self.login_details()
         # initialize Reddit object
         return praw.Reddit(client_id     = login.get('client_id'),
                            client_secret = login.get('client_secret'),
                            user_agent    = login.get('user_agent'),
-                           user_name     = login.get('user_name'),
+                           user_name     = login.get('username'),
                            password      = login.get('password')
                            )
 
@@ -72,3 +83,39 @@ class IncompleteLoginDetailsError(Exception):
     Custom exception for incorrect credentials file structure
     '''
     pass
+
+if __name__ == '__main__':
+    # initialize bot
+    nvbot = NoVowelsBot('credentials.json')
+    # get reddit instance
+    reddit = nvbot.init_bot()
+    # define which subreddit(s) we're looking at
+    subreddits = reddit.subreddit('+'.join(SUBREDDITS))
+
+    # main loop for bot
+    # collect todays top posts
+    no_posts = 0
+    posts = []
+    for submission in subreddits.top(time_filter='day'):
+        if submission.score >= SCORE_THRESHOLD and \
+           submission.created_utc > YDAY and \
+           submission.is_self:
+            posts.append({'title': submission.title,
+                          'text' : submission.selftext,
+                          'id'  : submission.id})
+            no_posts += 1
+            if no_posts > NO_OF_POSTS:
+                break
+    
+    # if no new top posts - quit
+    if posts == []:
+        sys.exit(0)
+
+    # "clean" posts
+    engine = BotEngine(reddit, posts)
+
+    clean_posts = engine.remove_vowels()
+    pprint(clean_posts)
+    # post on /r/fckvwls
+    fckvwls = reddit.subreddit('fckvwls')
+    engine.create_posts(fckvwls)
