7d680d29a2c8734c7b27512055c017f0f7d4d224
Peter Bocz <bocz.petya@gmail.com>
2019-05-18 23:14:48 +0100

    Restructured tests according to changes

--- tests/botengine_test.py
+++ tests/botengine_test.py
@@ -1,25 +1,29 @@
 import unittest
-from modules.BotEngine import Reader, PostTransformer
+from modules.BotEngine import BotEngine
 from NoVowelsBot import NoVowelsBot
 
 class BotEngineTest(unittest.TestCase):
-    @unittest.skip
-    def testReader_get_top(self):
-        bot = NoVowelsBot('credentials.json')
-        reddit = bot.reddit_instance()
-        reader = Reader(reddit, 'NoVowelBotTest')
-        top_results = reader.get_top_posts(1)[0]
-
-        self.assertEqual(top_results['title'], 'NoVowelBotTest has been created')
-        self.assertEqual(top_results['text'] , 'This is a test environment for the NoVowelBot')
-    @unittest.skip
-    def testTransform_1_remove_vowel(self):
+    
+    def test_remove_vowels(self):
         posts = [{'title': 'Title with a vowel for you',
-                  'text': 'Does it have all the vowels? Only you know.'}]
-        transform_obj = PostTransformer(posts)
-        print(transform_obj.remove_vowels())
+                  'text': 'Does it have all the vowels? Only you know.',
+                  'id': 0}]
+        engine = BotEngine(None, posts)
+        self.assertEqual(engine.remove_vowels(),
+                        [{'title': 'Ttl wth vwl fr y',
+                         'text': 'Ds t hv ll th vwls? nly y knw.',
+                         'id': 0}])
+
+    def test_add_comment(self):
+        nvbot = NoVowelsBot('credentials.json')
+        reddit = nvbot.init_bot()
+        engine = BotEngine(reddit, [])
+        engine.add_comment(reddit.subreddit('NoVowelBotTest'),
+                           'bq4v8d')
+
+
     @unittest.skip
-    def testPoster_1(self):
+    def test_create_post(self):
         pass
 
 if __name__ == '__main__':
