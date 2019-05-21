8559ff9f92457ec812af7adc20000ebade12383a
Peter Bocz <bocz.petya@gmail.com>
2019-05-21 23:04:57 +0100

    Redone structure

--- NoVowelsBot.py
+++ NoVowelsBot.py
@@ -1,15 +1,15 @@
 import praw
+import re
 import json
 from datetime import datetime, timedelta
 import sys
 from pprint import pprint
 
-from modules.BotEngine import BotEngine
-
 # constants
 SUBREDDITS = ['all']
 SCORE_THRESHOLD = 1000
-YDAY = (datetime.today() - timedelta(days=1)).timestamp() # yesterday 00:00 in UNIX timestamp
+DAYS = 1
+SINCE = (datetime.today() - timedelta(days=DAYS)).timestamp() # yesterday 00:00 in UNIX timestamp
 NO_OF_POSTS = 5
 
 # getting login information
@@ -18,7 +18,7 @@ class NoVowelsBot():
     def __init__(self, login_info):
         self.login_info = login_info
 
-    def login_details(self):
+    def get_login_details(self):
         '''
         Return the login info as a dictionary.
         It contains the following keys:
@@ -55,7 +55,7 @@ class NoVowelsBot():
         return login
     
     def init_bot(self):
-        login = self.login_details()
+        login = self.get_login_details()
         # initialize Reddit object
         return praw.Reddit(client_id     = login.get('client_id'),
                            client_secret = login.get('client_secret'),
@@ -63,6 +63,7 @@ class NoVowelsBot():
                            user_name     = login.get('username'),
                            password      = login.get('password')
                            )
+
 
 class LoginFileNotFound(Exception):
     '''
@@ -84,6 +85,67 @@ class IncompleteLoginDetailsError(Exception):
     '''
     pass
 
+def remove_vowels(post):
+    '''
+    Removes vowels from a post's title and selftext
+    '''
+    # compile 1 or more of the vowels ignoring the case
+    vowels  = re.compile('[aeiou]', re.I)
+
+    title = re.sub(vowels, '', post['title'])
+    text  = re.sub(vowels, '', post['text'])
+    return {'title': ' '.join(title.split()),
+            'text' : ' '.join(text.split()),
+            'id'   : post['id']}
+
+def get_posts(sub, score, since_day, no_posts):
+    # collect todays top posts
+    posts_collected = 0
+    posts = []
+    for submission in sub.top(time_filter='day'):
+        if submission.score >= score and \
+        submission.created_utc > since_day and \
+        submission.is_self:
+            posts.append({'title': submission.title,
+                        'text' : submission.selftext,
+                        'id'  : submission.id})
+            posts_collected += 1
+            if posts_collected > no_posts:
+                break
+    # if no new top posts - quit
+    if posts == []:
+        return None
+    else:
+        return posts
+
+def create_posts(reddit, posts):
+    '''
+    Creates a post on the subreddit specified.
+    
+    subreddit -> a Subreddit object
+    '''
+
+    def add_comment(sub, submission_id):
+        '''
+        Adds a comment to the subreddit where the post was read from
+
+        submission_id -> str - used to create a Submission object
+        '''
+
+        submission = reddit.submission(id=submission_id)
+        text = '''This post was featured in /r/fckvwls.   
+    We removed the unnecessary vowels because to be honest they're disgusting.   
+
+    ^(See my source code at https://github.com/analphagamma/NoVowelsBot)'''
+
+        submission.reply(text)
+
+    fckvwls = reddit.subreddit('fckvwls')
+    for post in posts:
+        fckvwls.submit(title=post['title'],
+                        selftext=post['text'])
+        add_comment(fckvwls, post['id'])
+
 if __name__ == '__main__':
     # initialize bot
     nvbot = NoVowelsBot('credentials.json')
@@ -91,31 +153,13 @@ if __name__ == '__main__':
     reddit = nvbot.init_bot()
     # define which subreddit(s) we're looking at
     subreddits = reddit.subreddit('+'.join(SUBREDDITS))
-
-    # main loop for bot
-    # collect todays top posts
-    no_posts = 0
-    posts = []
-    for submission in subreddits.top(time_filter='day'):
-        if submission.score >= SCORE_THRESHOLD and \
-           submission.created_utc > YDAY and \
-           submission.is_self:
-            posts.append({'title': submission.title,
-                          'text' : submission.selftext,
-                          'id'  : submission.id})
-            no_posts += 1
-            if no_posts > NO_OF_POSTS:
-                break
-    
-    # if no new top posts - quit
-    if posts == []:
+    posts = get_posts(subreddits, SCORE_THRESHOLD, SINCE, NO_OF_POSTS)
+    # exit if no posts meet the requirements
+    if not posts:
         sys.exit(0)
 
-    # "clean" posts
-    engine = BotEngine(reddit, posts)
-
-    clean_posts = engine.remove_vowels()
+    clean_posts = list(map(remove_vowels, posts))
     pprint(clean_posts)
     # post on /r/fckvwls
-    fckvwls = reddit.subreddit('fckvwls')
-    engine.create_posts(fckvwls)
+    
+    create_posts(reddit, clean_posts)
--- tests/bot_test.py
+++ tests/bot_test.py
@@ -3,25 +3,53 @@ from NoVowelsBot import *
 
 class BotTest(unittest.TestCase):
     
-    def test1_file_not_found(self):
+    def test_init_1_file_not_found(self):
         '''
         Test whether the right exception is thrown if the
         login file is not present.
         '''
         bot = NoVowelsBot('notexisting.json')
-        self.assertRaises(LoginFileNotFound, lambda: bot.login_details())
+        self.assertRaises(LoginFileNotFound, lambda: bot.get_login_details())
 
-    def test2_invalid_json(self):
+    def test_init_2_invalid_json(self):
         bot = NoVowelsBot('bad_file.json')
-        self.assertRaises(NotJSONFileError, lambda: bot.login_details())
+        self.assertRaises(NotJSONFileError, lambda: bot.get_login_details())
 
-    def test3_incomplete_login_empty(self):
+    def test_init_3_incomplete_login_empty(self):
         bot = NoVowelsBot('bad_credentials_empty.json')
-        self.assertRaises(IncompleteLoginDetailsError, lambda: bot.login_details())
+        self.assertRaises(IncompleteLoginDetailsError, lambda: bot.get_login_details())
 
-    def test4_incomplete_login_missing(self):
+    def test_init_4_incomplete_login_missing(self):
         bot = NoVowelsBot('bad_credentials_missing.json')
-        self.assertRaises(IncompleteLoginDetailsError, lambda: bot.login_details())
+        self.assertRaises(IncompleteLoginDetailsError, lambda: bot.get_login_details())
+    
+    def test_init_5_successful_login(self):
+        bot = NoVowelsBot('credentials.json')
+        reddit = bot.init_bot()
+        self.assertNotEqual(reddit.user.me(), None)
+    
+    def test_remove_vowels(self):
+        post = {'title': 'Title with a vowel for you',
+                  'text': 'Does it have all the vowels? Only you know.',
+                  'id': 0}
+        self.assertEqual(remove_vowels(post),
+                        {'title': 'Ttl wth vwl fr y',
+                         'text': 'Ds t hv ll th vwls? nly y knw.',
+                         'id': 0})
+
+    def test_get_posts_from_test_subreddit(self):
+        SCORE_THRESHOLD = 1
+        DAYS = 14
+        SINCE = (datetime.today() - timedelta(days=DAYS)).timestamp()
+        NO_OF_POSTS = 5
+        nvbot = NoVowelsBot('credentials.json')
+        reddit = nvbot.init_bot()
+        test_sub = reddit.subreddit('NoVowelBotTest')
+        posts = get_posts(test_sub, SCORE_THRESHOLD, SINCE, NO_OF_POSTS)
+        self.assertEqual(posts[0],
+                        {'title': 'NoVowelBotTest has been created',
+                          'text' : 'This is a test environment for the NoVowelBot',
+                          'id'   : 'bq4v8d'})
                           
 if __name__ == '__main__':
     unittest.main()
